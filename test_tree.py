882d05eb0e4fad1dd53af933dff330786902be3f
Austin <ahendricks2@cocc.edu>
2018-10-29 20:12:44 -0700

    Add failing test.

---
+++ test_tree.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestTree(unittest.TestCase):
+    
+    def test_failure(self):
+        self.fail("Intentional failure.")
+
+if __name__ == '__main__':
+    unittest.main()
