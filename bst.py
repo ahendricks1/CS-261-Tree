eccb365e90d770a999303258b3c3ffa3160bb78d
Austin <ahendricks2@cocc.edu>
2018-10-31 19:38:05 -0700

    Added initialization tests.

---
+++ bst.py
@@ -0,0 +1,6 @@
+class BinarySearchTree:
+    
+    def __init__(self, value = None, left = None, right = None):
+        self.value = value
+        self.left = left
+        self.right = right
--- test_bst.py
+++ test_bst.py
@@ -1,9 +1,36 @@
+#python -m unittest test_bst
+#start every test with 'test_'
+
 import unittest
+from bst import BinarySearchTree
 
 class TestBinarySearchTreeTree(unittest.TestCase):
+
+    """
+    Initialization
+    """
     
-    def test_failure(self):
-        self.fail("Could not instantiate BinarySearchTree.")
+    def test_instantiation(self):
+        try:
+            BinarySearchTree()
+        except NameError:
+            self.fail("Could not instantiate BinarySearchTree.")
+
+    def test_instantiation_with_value(self):
+        fake_value = "FAKE"
+        bst = BinarySearchTree(fake_value)
+        self.assertEqual(fake_value, bst.value)
+
+    def test_instantiation_with_value_left_right(self):
+        fake_value = "FAKE"
+        fake_child = "FAKE"
+        bst = BinarySearchTree(fake_value, fake_child, fake_child)
+        self.assertEqual(fake_child, bst.left)
+        self.assertEqual(fake_child, bst.right)
+
+    """
+    Insert
+    """
 
 if __name__ == '__main__':
     unittest.main()
