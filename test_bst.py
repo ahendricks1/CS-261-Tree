37ee3e113ae498f29eb4c088357a7b81fbbe5815
Austin <ahendricks2@cocc.edu>
2018-11-01 17:12:05 -0700

    Added passing insert left/right tests.

--- test_bst.py
+++ test_bst.py
@@ -39,18 +39,14 @@ class TestBinarySearchTreeTree(unittest.TestCase):
         bst.insert(fake_child)
 
     def test_insert_with_value_left(self):
-        fake_value = "FAKE"
-        fake_child_left = BinarySearchTree()
-        fake_child_right = BinarySearchTree()
-        bst = BinarySearchTree(fake_value, fake_child_left, fake_child_right)
+        bst = BinarySearchTree("FAKE", BinarySearchTree(), BinarySearchTree())
         bst.insert_left(41)
         self.assertEqual(41, bst.left.value)
     
     def test_insert_with_value_right(self):
-        fake_value = "FAKE"
         fake_child_left = BinarySearchTree()
         fake_child_right = BinarySearchTree()
-        bst = BinarySearchTree(fake_value, fake_child_left, fake_child_right)
+        bst = BinarySearchTree("FAKE", fake_child_left, fake_child_right)
         bst.insert_right(41)
         self.assertEqual(41, bst.right.value)
 
